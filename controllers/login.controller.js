82542dcef24790e8696dc45de6f4e8a8ee2f8936
NSQA <nguyensyquanganh@gmail.com>
2020-04-27 23:23:00 +0700

    Create signed cookie

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,8 +1,14 @@
-
+const users = 
+    {
+        email: 'nguyensyquanganh@gmail.com',
+        password: '12022020',
+        status: 'verified'
+    };
 module.exports.getLogin = (req, res) => {
     res.render('index');
 }
 
 module.exports.postLogin = (req, res) => {
+    res.cookie('status', users.status, { signed: true });
     res.redirect('/user/home');
 }
