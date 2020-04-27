eec120f4ee257ab5641ce2ee1017ab503728a7d7
NSQA <nguyensyquanganh@gmail.com>
2020-04-27 23:26:28 +0700

    Add authentication for home page

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,15 +2,16 @@ const express = require('express');
 const routeUser = express.Router();
 const weatherController = require('../controllers/weather.controller');
 const userController = require('../controllers/user.controller');
+const authLogin = require('../controllers/auth.controller');
 
-routeUser.get('/home', userController.getHome);
+routeUser.get('/home', authLogin.requireAuth, userController.getHome);
 
 routeUser.get('/signup', userController.getSignup);
 
 routeUser.post('/signup', userController.postSignup);
 
 
-routeUser.get('/weather', weatherController.getWeather);
+routeUser.get('/weather', authLogin.requireAuth, weatherController.getWeather);
 
 
 module.exports = routeUser;
