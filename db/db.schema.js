8a03c20da2915ab42e2349fd76b2eb9cf1fa7049
NSQA <nguyensyquanganh@gmail.com>
2020-04-28 16:21:04 +0700

    Add "required" option for fields in schema

--- db/db.schema.js
+++ db/db.schema.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: String,
-    password: String,
-    status: String
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    status: {type: String, required: true}
 });
 
 module.exports = userSchema;
